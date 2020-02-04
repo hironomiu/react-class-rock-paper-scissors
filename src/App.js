100b79d87d81c7b6d10d97e4a4e56950c780ab25
hironomiu <hironomiu@gmail.com>
2020-02-04 17:59:22 +0900

    commited

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let title = "じゃんけん";
+
+function mappingState(state) {
+  return state;
 }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{title}</h1>
+        <hr />
+        <OpponentHand />
+        <Result />
+        <MyHand />
+        <Button />
+      </div>
+    );
+  }
+}
+
+App = connect()(App);
+
+class OpponentHand extends Component {
+  render() {
+    return (
+      <img src={`${process.env.PUBLIC_URL}/images/` + this.props.opsrc + ".png"} alt={this.props.opsrc} />
+    );
+  }
+}
+
+OpponentHand = connect(mappingState)(OpponentHand);
+
+class Result extends Component {
+  render() {
+    return (
+      <h2>{this.props.judge}</h2>
+    );
+  }
+}
+
+Result = connect(mappingState)(Result);
+
+class MyHand extends Component {
+  render() {
+    return (
+      <img src={`${process.env.PUBLIC_URL}/images/` + this.props.src + ".png"} alt={this.props.src} />
+    );
+  }
+}
+
+MyHand = connect(mappingState)(MyHand);
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.rockAction = this.rockAction.bind(this);
+    this.scissorsAction = this.scissorsAction.bind(this);
+    this.paperAction = this.paperAction.bind(this);
+  }
+
+  rockAction(e) {
+    this.props.dispatch({ type: 'rock' });
+  }
+
+  scissorsAction(e) {
+    this.props.dispatch({ type: 'scissors' });
+  }
+
+  paperAction(e) {
+    this.props.dispatch({ type: 'paper' });
+  }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <button onClick={this.rockAction} value="rock" ><img src={`${process.env.PUBLIC_URL}/images/rock.png`} alt="rock" /></button>
+        <button onClick={this.scissorsAction} value="scissors" ><img src={`${process.env.PUBLIC_URL}/images/scissors.png`} alt="scissors" /></button>
+        <button onClick={this.paperAction} value="paper" ><img src={`${process.env.PUBLIC_URL}/images/paper.png`} alt="paper" /></button>
+      </div>
+    );
+  }
+}
+
+Button = connect()(Button);
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HAND_TYPE = ["rock", "scissors", "paper"];
+const JUDGE = ["Draw", "Win", "Lose"];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let state_value = {
+    src: HAND_TYPE[0],
+    opsrc: HAND_TYPE[0],
+    judge: JUDGE[0],
+}
+
+function hand(state = state_value, action) {
+    let opsrc = HAND_TYPE[Math.floor(Math.random() * 3)];
+    let result = judge(action.type, opsrc);
+
+    if (action.type !== HAND_TYPE[0] && action.type !== HAND_TYPE[1] && action.type !== HAND_TYPE[2]) return state;
+
+    return {
+        src: action.type,
+        opsrc: opsrc,
+        judge: result
+    }
+}
+
+function judge(myHand, opponentHand) {
+    if (myHand === opponentHand) {
+        return JUDGE[0];
+    }
+    if ((myHand === HAND_TYPE[0] && opponentHand === HAND_TYPE[1]) ||
+        (myHand === HAND_TYPE[1] && opponentHand === HAND_TYPE[2]) ||
+        (myHand === HAND_TYPE[2] && opponentHand === HAND_TYPE[0])) {
+        return JUDGE[1];
+    }
+    return JUDGE[2];
+}
+
+let store = createStore(hand);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
